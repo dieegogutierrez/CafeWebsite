cfe3aaf62b0c6a0dcebad18bc6ca369e0488b3e7
dieegogutierrez <dieego_gutierrez@hotmail.com>
2023-11-05 12:40:19 -0500

    Db creation

--- website/models.py
+++ website/models.py
@@ -50,7 +50,5 @@ class User(db.Model, UserMixin):
         return f"User('{self.username}', '{self.email}')"
 
 
-with app.app_context():
-    db.create_all()
-
-
+# with app.app_context():
+#     db.create_all()
